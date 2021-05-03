e9190f7f838a65deabcac99f42ccfcc51104b7e5
gohdg <gohdg@naver.com>
2021-05-03 22:57:52 +0900

    finished lecture 07

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import Layout from "../comps/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} /> {/* component들이 렌더링된다. */}
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,19 @@
 const about = () => {
   return (
     <div>
-      <h2>About Page...</h2>
+      <h1>About page...</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, ex?
+        Nisi ea beatae dolorem corporis laboriosam nesciunt atque molestiae
+        excepturi, doloribus vel cum cupiditate, voluptates aut exercitationem
+        odio minima mollitia.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, ex?
+        Nisi ea beatae dolorem corporis laboriosam nesciunt atque molestiae
+        excepturi, doloribus vel cum cupiditate, voluptates aut exercitationem
+        odio minima mollitia.
+      </p>
     </div>
   );
 };
